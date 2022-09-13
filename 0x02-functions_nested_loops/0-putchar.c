4ebb8cda6b56893d9d099847c93e51afb6fa4b67
Nasonex2 <nasonexdesk@gmail.com>
2022-09-13 11:20:03 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the string "main.h" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "main.h";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
