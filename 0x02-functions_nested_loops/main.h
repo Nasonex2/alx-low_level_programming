958dc3ac1cd256755fe332a62543398b55fd34ea
Nasonex2 <nasonexdesk@gmail.com>
2022-09-13 15:04:35 -0700

    root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# ls main.h  README.md root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# vi main.h root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# ls main.h  README.md root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# cat main.h
    int _putchar(char c);
    void print_alphabet(void);
    void print_alphabet_x10(void);
    int _islower(int c);
    int _isalpha(int c);
    int print_sign(int n);
    int _abs(int);
    int print_last_digit(int);
    void jack_bauer(void);
    void times_table(void);
    int add(int, int);
    void print_to_98(int n);
    void print_times_table(int n);
    
    root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# vi 0-putchar.c
    root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# ls
    0-putchar.c  main.h  README.md
    root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# git add .
    root@9b7432eaca9a:~/alx-low_level_programming/0x02-functions_nested_loops# git commit -m

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
 
 
-int _putchar(int c);
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,4 +15,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
